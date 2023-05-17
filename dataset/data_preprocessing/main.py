311abdd1c8f98564db6782336e409c2a0a590bf4
capsaicinoids <frizaaditya09@gmail.com>
2023-05-17 09:18:17 +0700

    Refactor codebase: Move functions into dedicated file and update folder structure

---
+++ dataset/data_preprocessing/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+from utils import data_preprocessing as dp
+import pandas as pd
+import numpy as np
+
+if __name__ == '__main__':
+    pond = 2
+    df = pd.read_csv(f"../raw/IoTPond{pond}.csv", index_col=0)
+    df = dp.drop_column(dp.search_columns(df), df)
+    df = dp.column_preprocessing(dp.search_columns(df), df)
+    df = dp.filter_column(dp.search_columns(df), df)
+    df = dp.rename_column(df)
+
+    df.to_csv(f'../processed/cleaned_IoTpond{pond}.csv', index=True)
+    print('DONE')
+
+
+"""
+    - Change the name of the column (Done)
+    - Delete column entry_id and population automatically (Done)
+    - Divide The Nitrate value with 100 (Done)
+"""
+
+
+
+""" # For Water Quality Parameters
+    - Clean The Dataset First
+    - Predict The zero value
+    - 1 hour interval AVG || 1 day Interval AVG
+
+"""
+
+
+""" # Catfish size Parameters
+    - 1 day Interval Avg
+    - Add Delta Column
+"""
---
+++ dataset/data_preprocessing/utils/data_preprocessing.py
@@ -0,0 +1,70 @@
+def search_columns(df) -> list:
+    """
+    Returns a list of columns from the given dataframe that contain any of the keywords bellow. The function takes a pandas dataframe as its only parameter and returns a list of strings.
+    """
+    keywords = ['temp', 'oxygen', 'ph', 'ammonia', 'nitrat', 'entry_id', 'population']
+    column_index = [col for col in df.columns if any(word in col.lower() for word in keywords)]
+
+    return column_index
+
+def drop_column(column_name: str, df):
+    """
+    This function drops a column from a pandas dataframe given its name and returns the updated dataframe.
+
+    Args:
+        column_name (str): The name of the column to be dropped.
+        df (pandas.DataFrame): The dataframe from which the column is to be dropped.
+
+    Returns:
+        pandas.DataFrame: The updated dataframe with the specified column dropped.
+    """
+    df.drop(columns=[column_name[0], column_name[-1]], inplace=True)
+    return df
+
+def column_preprocessing(column_name: str, df):
+    """
+    Apply a preprocessing step to a given column of a pandas DataFrame. 
+
+    Args:
+        column_name (str): Name of the column to be preprocessed. 
+        df (pandas.DataFrame): DataFrame containing the target column.
+
+    Returns:
+        pandas.DataFrame: The updated DataFrame with the preprocessed column. 
+    """
+    df[column_name[4]] = df[column_name[4]].apply(lambda x: x / 100)
+    return df
+
+def filter_column(column_name: str, df) :
+    df.loc[:, column_name[0]] *= (df.loc[:, column_name[0]].between(20, 35)) # Temperature
+    df.loc[:, column_name[1]] *= (df.loc[:, column_name[1]].between(0, 10)) # Dissolved Oxygen
+    df.loc[:, column_name[2]] *= (df.loc[:, column_name[2]].between(1, 14)) # pH
+    df.loc[:, column_name[3]] *= (df.loc[:, column_name[3]].between(0, 1)) # Ammonia
+    df.loc[:, column_name[4]] *= (df.loc[:, column_name[4]].between(0, 20)) # Nitrate
+
+    return df
+
+def rename_column(df):
+    """
+    Renames the columns of a given pandas DataFrame object. The function modifies the DataFrame object in place.
+
+    Parameters:
+    -----------
+    df : pandas.DataFrame
+        The DataFrame object to rename its columns.
+
+    Returns:
+    --------
+    pandas.DataFrame
+        The modified DataFrame object with the renamed columns.
+    """
+    df.rename(columns={df.columns[0]: 'temperature_c',
+                       df.columns[1]: 'turbidity_ntu',
+                       df.columns[2]: 'do',
+                       df.columns[3]: 'ph',
+                       df.columns[4]: 'ammonia',
+                       df.columns[5]: 'nitrate',
+                       df.columns[6]: 'fish_length_cm',
+                       df.columns[7]: 'fish_weight_g'}, inplace=True)
+
+    return df
