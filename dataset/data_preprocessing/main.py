4e5e0a22f54c4a6a2e9ccd50dba7f37294e8a5aa
capsaicinoids <frizaaditya09@gmail.com>
2023-05-17 20:27:01 +0700

    Fix Major Fatal Issues

--- dataset/data_preprocessing/main.py
+++ dataset/data_preprocessing/main.py
@@ -4,22 +4,21 @@ from utils import data_preprocessing as dp
 import pandas as pd
 
 if __name__ == '__main__':
-    pond = 2
+    pond = 3
 
     # Uncomment this if you want to clean the data
-    # df = pd.read_csv(f"../raw/IoTPond{pond}.csv", index_col=0, parse_dates=True)
-    # dp.wrap_it_up(pond, df)
-
+    df = pd.read_csv(f"../raw/IoTPond{pond}.csv", index_col=0, parse_dates=True)
+    dp.wrap_it_up(pond, df)
 
     # Uncomment this if you want to change the interval of time into an hour
-    df_list = []
-    for i in range(1, 5):
-        df = pd.read_csv(f'../processed/filled_IoTPond{pond}_part{i}.csv',index_col=0, parse_dates=[0])
-        df_list.append(df)
-    df = pd.concat(df_list)
+    # df_list = []
+    # for i in range(1, 5):
+    #     df = pd.read_csv(f'../processed/filled_IoTPond{pond}_part{i}.csv',index_col=0, parse_dates=[0])
+    #     df_list.append(df)
+    # df = pd.concat(df_list)
 
-    df = df.resample('1H').mean() # Specify The Interval
-    df.to_csv(f'../processed/one_hour_IoTPond{pond}.csv', index=True)
+    # df = df.resample('1H').mean() # Specify The Interval
+    # df.to_csv(f'../processed/one_hour_IoTPond{pond}.csv', index=True)
 
 """
     - (Done) Change the name of the column
@@ -32,8 +31,7 @@ if __name__ == '__main__':
 """ # For Water Quality Parameters
     - (Done) Clean The Dataset First
     - (Done) Predict The zero value
-    - 1 hour interval AVG || 1 day Interval AVG
-
+    - (Done) 1 hour interval AVG || 1 day Interval AVG
 """
 
 
--- dataset/data_preprocessing/utils/data_preprocessing.py
+++ dataset/data_preprocessing/utils/data_preprocessing.py
@@ -1,30 +1,33 @@
-import pandas as pd
 import numpy as np
 
-def search_columns(df) -> list:
+def search_columns(df) -> dict:
     """
-    Returns a list of columns from the given dataframe that contain any of the keywords bellow. The function takes a pandas dataframe as its only parameter and returns a list of strings.
+    Returns a dictionary with keywords as keys and the corresponding indices of columns from the given dataframe as values. The function takes a pandas dataframe as its only parameter and returns a dictionary.
     """
-    keywords = ['temp', 'oxygen', 'ph', 'ammonia', 'nitrat', 'entry_id', 'population']
-    column_index = [col for col in df.columns if any(word in col.lower() for word in keywords)]
+    keywords = {'temp', 'turbidity', 'oxygen', 'ph', 'ammonia', 'nitrat', 'entry_id', 'population', 'date', 'length', 'weight'}
+    lower_cols = [col.lower() for col in df.columns]
+    column_dict = {word: next((idx for idx, lower_col in enumerate(lower_cols) if word in lower_col), None) for word in keywords}
 
-    return column_index
+    return column_dict
 
-def drop_column(column_name: str, df):
+def drop_column(column_dict: dict, df):
     """
     This function drops a column from a pandas dataframe given its name and returns the updated dataframe.
-
+``
     Args:
-        column_name (str): The name of the column to be dropped.
-        df (pandas.DataFrame): The dataframe from which the column is to be dropped.
+        column_dict (dict): A dictionary of column names and their corresponding indices.
+        df (pandas.DataFrame): The dataframe from which the columns are to be dropped.
 
     Returns:
-        pandas.DataFrame: The updated dataframe with the specified column dropped.
+        pandas.DataFrame: The updated dataframe with the specified columns dropped.
     """
-    df.drop(columns=[column_name[0], column_name[-1]], inplace=True)
+    indices_to_drop = [column_dict.get('date'), column_dict.get('entry_id'), column_dict.get('population')]
+    df.drop(columns=[col for col_idx, col in enumerate(df.columns) if col_idx in indices_to_drop], inplace=True)
+
     return df
 
-def column_preprocessing(column_name: str, df):
+
+def column_preprocessing(column_dict: dict, df):
     """
     Apply a preprocessing step to a given column of a pandas DataFrame. 
 
@@ -35,52 +38,33 @@ def column_preprocessing(column_name: str, df):
     Returns:
         pandas.DataFrame: The updated DataFrame with the preprocessed column. 
     """
-    df[column_name[4]] = df[column_name[4]].apply(lambda x: x / 100)
-    return df
-
-def filter_column(column_name: str, df) :
-    df.loc[:, column_name[0]] = df.loc[:, column_name[0]].apply(lambda x: x if 20 <= x <= 35 else None) # Temperature
-    df.loc[:, column_name[1]] = df.loc[:, column_name[1]].apply(lambda x: x if 0 <= x <= 10 else None) # Dissolved Oxygen
-    df.loc[:, column_name[2]] = df.loc[:, column_name[2]].apply(lambda x: x if 1 <= x <= 14 else None) # pH
-    df.loc[:, column_name[3]] = df.loc[:, column_name[3]].apply(lambda x: x if 0 <= x <= 1 else None) # Ammonia
-    df.loc[:, column_name[4]] = df.loc[:, column_name[4]].apply(lambda x: x if 0 <= x <= 20 else None) # Nitrate
+    df.iloc[:, column_dict['nitrat']] = df.iloc[:, column_dict['nitrat']].apply(lambda x: x / 100)
 
     return df
 
-def rename_column(df):
-    """
-    Renames the columns of a given pandas DataFrame object. The function modifies the DataFrame object in place.
-
-    Parameters:
-    -----------
-    df : pandas.DataFrame
-        The DataFrame object to rename its columns.
-
-    Returns:
-    --------
-    pandas.DataFrame
-        The modified DataFrame object with the renamed columns.
-    """
-    df.rename(columns={df.columns[0]: 'temperature_c',
-                       df.columns[1]: 'turbidity_ntu',
-                       df.columns[2]: 'do',
-                       df.columns[3]: 'ph',
-                       df.columns[4]: 'ammonia',
-                       df.columns[5]: 'nitrate',
-                       df.columns[6]: 'fish_length_cm',
-                       df.columns[7]: 'fish_weight_g'}, inplace=True)
+def filter_column(column_dict: dict, df) :
+    df.iloc[:, column_dict['temp']] = df.iloc[:, column_dict['temp']].apply(lambda x: x if 20 <= x <= 35 else None) # Temperature
+    df.iloc[:, column_dict['oxygen']] = df.iloc[:, column_dict['oxygen']].apply(lambda x: x if 0 <= x <= 10 else None) # Dissolved Oxygen
+    df.iloc[:, column_dict['ph']] = df.iloc[:, column_dict['ph']].apply(lambda x: x if 1 <= x <= 14 else None) # pH
+    df.iloc[:, column_dict['ammonia']] = df.iloc[:, column_dict['ammonia']].apply(lambda x: x if 0 <= x <= 1 else None) # Ammonia
+    df.iloc[:, column_dict['nitrat']] = df.iloc[:, column_dict['nitrat']].apply(lambda x: x if 0 <= x <= 20 else None) # Nitrate
 
     return df
 
-import numpy as np
-import pandas as pd
 
-def wrap_it_up(pond: int, df: pd.DataFrame) -> None:
+def rename_column(column_dict: dict, df):
+    df.rename(columns={df.columns[v]: k for k, v in column_dict.items() if v is not None}, inplace=True)
+
+    return df
+
+import os
+
+def wrap_it_up(pond: int, df) -> None:
     """
     Preprocesses and splits a DataFrame into 4 parts, saving each part as a CSV file.
 
     Args:
-        pond (int): The pond number associated with the DataFrame.
+        pond (int): The pond that you want to choose.
         df (pd.DataFrame): The DataFrame to preprocess and split.
     Returns:
         None
@@ -95,11 +79,16 @@ def wrap_it_up(pond: int, df: pd.DataFrame) -> None:
     df = filter_column(search_columns(df), df)
 
     # Rename columns
-    df = rename_column(df)
+    df = rename_column(search_columns(df), df)
+
+    # Check if directory exists, if not create it
+    directory = f"../processed/IoTPond{pond}"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
     # Split DataFrame into 4 parts and save each part as a CSV file
     df_split = np.array_split(df, 4)
     for i, split in enumerate(df_split):
-        split.to_csv(f"../processed/cleaned_IoTPond{pond}_part{i+1}.csv", index=True)
+        split.to_csv(f"{directory}/cleaned_IoTPond{pond}_part{i+1}.csv", index=True)
 
     print('DONE')
