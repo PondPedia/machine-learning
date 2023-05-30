443f30c6eee9014d0241585cc45650fa02d4769d
capsaicinoids <frizaaditya09@gmail.com>
2023-05-30 11:29:08 +0700

    Tidying The Structure of the Code

--- dataset/data_preprocessing/main.py
+++ dataset/data_preprocessing/main.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python
-
-from utils import data_preprocessing as dp
 import pandas as pd
+from utils import data_cleaning, data_sampling, get_user_inputs
 
 if __name__ == "__main__":
-    pond = 2
-    split_amount = 1
+    options, choice, pond, split_amount, interval = get_user_inputs()
+    df = pd.read_csv(f"../raw/IoTPond{pond}.csv", index_col=0, parse_dates=True)
 
-    # Uncomment this if you want to clean the data
-    # df = pd.read_csv(f"../raw/IoTPond{pond}.csv", index_col=0, parse_dates=True)
-    # dp.wrap_it_up(pond, df, split_amount=split_amount)
-
-    # Uncomment this if you want to change the interval of time into an hour
-    df_list = []
-    for i in range(1, split_amount + 1):
-        df = pd.read_csv(
-            f"../processed/IoTPond{pond}/cleaned_IoTPond{pond}_part{i}.csv",
-            index_col=0,
-            parse_dates=[0],
-        )
-        df_list.append(df)
-    df = pd.concat(df_list)
-
-    # Specify The Interval
-    df_resampled = df.resample("6H")
-    df_mean = df_resampled.mean().iloc[:, 0:-2]
-    df_max = df_resampled.max().iloc[:, -2:]  
-    df_mean.join(df_max).to_csv(f"../processed/IoTPond{pond}/6_hours_IoTPond{pond}.csv", index=True)
+    if (choice == 1):
+        data_cleaning.wrap_it_up(pond, df, split_amount)
+    elif (choice == 2):
+        data_sampling.sampling_data(split_amount, pond, interval)
+    else:
+        print('Invalid Choice')
