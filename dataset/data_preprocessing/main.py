72bfcaadcbf496ba547123bfdb8b88e2f3186a23
capsaicinoids <frizaaditya09@gmail.com>
2023-05-30 10:46:30 +0700

    Remove Unnecessary Comments

--- dataset/data_preprocessing/main.py
+++ dataset/data_preprocessing/main.py
@@ -27,39 +27,3 @@ if __name__ == "__main__":
     df_mean = df_resampled.mean().iloc[:, 0:-2]
     df_max = df_resampled.max().iloc[:, -2:]  
     df_mean.join(df_max).to_csv(f"../processed/IoTPond{pond}/6_hours_IoTPond{pond}.csv", index=True)
-
-"""
-    - (Done) Change the name of the column
-    - (Done) Delete column entry_id and population automatically
-    - (Done) Divide The Nitrate value with 100
-"""
-
-
-""" # For Water Quality Parameters
-    - (Done) Clean The Dataset First
-    - (Done) Predict The zero value
-    - (Done) 1 hour interval AVG || 1 day Interval AVG
-"""
-
-
-""" # Catfish size Parameters
-    - 1 day Interval Avg
-    - Add Delta Column
-    - water quality parameters (avg)
-    - fish size parameters (max)
-"""
-
-"""
-    1. Friza
-    - Email 
-    - Create a model with combined version of IoTPond 1 and 2 (6 Hours Intervals)
-    
-
-    2. Irvan
-    - Keep Playing with 1 Day Interval
-    - Python script for filling missing values automatically
-
-    3. Astrid
-    - Preprocessing Data 3 & 4 (8 Hours Intervals)
-    - 
-"""
